2384ceced8e4642a7c96dc3a27c9cc70551be253
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-10-20 18:28:53 +0800

    Updated SQL query

--- WebApp/routes/login.js
+++ WebApp/routes/login.js
@@ -7,11 +7,11 @@ var conn = mysql.createConnection(databaseConfig);
 
 router.get('/loginCheck/', function(req,res){
     console.log(req.query.password);
-    conn.query("Select USER_PASSWORD from participants where USER_NAME=?", req.query.username, function(err, result, fields){
+    conn.query("Select USER_PASSWORD from Participants where USER_NAME=?", req.query.username, function(err, result, fields){
         if (err){
             res.send('username or password is wrong');
         }
-        console.log(result)
+        // console.log(result)
         var password = result[0].USER_PASSWORD;
         console.log("pass: " + password);
         if(password === req.query.password){
