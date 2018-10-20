b914617ca0fcdb15280f6992488c15f7987245ad
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-10-20 19:50:57 +0800

    Updated to include registering of new users.

--- WebApp/app.js
+++ WebApp/app.js
@@ -3,8 +3,10 @@ var express = require('express');
 
 var loginRouter = require('./routes/login.js');
 
+//declares the app
 var app = express();
 
+//declares the routes available
 app.use('/login', loginRouter);
 
 module.exports = app;
