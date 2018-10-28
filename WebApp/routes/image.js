7b1feda5b3c924a7908b650711b1bbed716ec19f
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-10-28 17:56:13 +0800

    Update WebApp/routes/image.js

--- WebApp/routes/image.js
+++ WebApp/routes/image.js
@@ -12,7 +12,8 @@ var databaseConfig = require('../config/mysqlconf.js');
 var conn = mysql.createConnection(databaseConfig);
 var submissionDir = './images/submission';
 //get number of data in the database
-var submission_index = conn.query('SELECT MAX(SUBMISSION_ID) as SUBMISSION_ID FROM SUBMISSION', function(err,data){
+var submission_index = 0;
+conn.query('SELECT MAX(SUBMISSION_ID) as SUBMISSION_ID FROM SUBMISSION', function(err,data){
     if(err){
         console.log(err);
     }
@@ -24,25 +25,27 @@ var submission_index = conn.query('SELECT MAX(SUBMISSION_ID) as SUBMISSION_ID FR
                 if (err){
                     console.log("error: " + err);
                 }
-                console.log(data2[0].count);
-                return data2[0].count;
+                // console.log(data2[0].count);
+                submission_index = data2[0].count;
         })
     } else{
-        console.log(data[0]);
-        return ++data[0].SUBMISSION_ID;
+        // console.log(data[0]);
+        var index = data[0].SUBMISSION_ID + 1;
+        submission_index = index;
     }
     
 
 })
 
-// console.log(submission_index);
+console.log("submission index: " + submission_index);
 
 router.post('/uploadSubmission', multipart({ uploadDir: submissionDir}), function(req,res){
     // console.log(req.files);
-    // var submission_id = submission_index++;
-    // var team_id = req.body.team_id;
-    // var trail_instance_id = req.body.trail_instance.id;
-    // var question = req.body.question
+    var submission_id = submission_index;
+    submission_index++;
+    var team_id = req.body.team_id;
+    var trail_instance_id = req.body.trail_instance_id;
+    var question_id = req.body.question_id
 
     // var question_id = conn.query('SELECT QUESTION_ID FROM SUBMISSION_QUESTION WHERE QUESTION = ?', question, function(err, data){
     //     if(err){
@@ -51,14 +54,13 @@ router.post('/uploadSubmission', multipart({ uploadDir: submissionDir}), functio
 
     //     return data[0].QUESTION_ID;
     // })
-    console.log(req);
+    // console.log(req);
 
     fs.readFile(req.files.image.path, function(err,data){
         // console.log(req.files.image.path);
         if(req.files){
-            res.status(200).send("Uploaded!!!");
+            res.send("Uploaded!!!");
         }
-
         var image_path = req.files.image.path
         var new_image_path = image_path.substring(
                             0,image_path.lastIndexOf('\\')+1)
@@ -70,29 +72,39 @@ router.post('/uploadSubmission', multipart({ uploadDir: submissionDir}), functio
         fs.rename(image_path, new_image_path, function(err){
             // res.send(err);
         });
-    })
 
-    // var query = 'INSERT INTO SUBMISSION(SUBMISSION_ID, SUBMISSION_IMAGE_URL, TEAM_ID, TRAIL_INSTANCE_ID, QUESTION_ID) VALUES (?,?,?,?,?)'
+        var query = 'INSERT INTO SUBMISSION(SUBMISSION_ID, SUBMISSION_IMAGE_URL, TEAM_ID, TRAIL_INSTANCE_ID, QUESTION_ID) VALUES (?,?,?,?,?)'
 
-    // console.log(submission_id);
-    // console.log(new_image_path);
-    // console.log(team_id);
-    // console.log(trail_instance_id);
-    // console.log(question_id);
+        // console.log("submission: " + submission_id);
+        // console.log("image url: " + new_image_path);
+        // console.log("team id: " + team_id);
+        // console.log("trail instance id: " + trail_instance_id);
+        // console.log("question id: " + question_id);
+        
     
-
-    // conn.query(query,[submission_id, new_image_path, team_id, trail_instance_id, question_id],function(err,data){
-    //     if (err){
-    //         res.send(err);
-    //     } else{
-    //         console.log('submission successfully loaded');
-    //     }
-    // })
-
-    // res.end('Submission Successful');
+        conn.query(query,[submission_id, new_image_path, team_id, trail_instance_id, question_id],function(err,data){
+            if (err){
+                console.log(err);
+            } else{
+                console.log('submission successfully loaded');
+            }
+        })
+    })
+    
 })
 
-// router.get('getAllSubmissions', function(req,res){
-//     var 
-// })
+router.get('/getAllSubmissions', function(req,res){
+    var query = 'SELECT SUBMISSION_IMAGE_URL FROM SUBMISSION'
+    conn.query(query, function(err, data){
+        if(err){
+            console.log(err);
+        } else{
+            for(var i=0; i<data.length; i++){
+                var image_url = data[i].SUBMISSION_IMAGE_URL;
+                console.log(image_url);
+            }
+        }
+
+    })
+})
 module.exports = router;
