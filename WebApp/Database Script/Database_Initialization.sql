ce2a48ee2791939d82585aca6084fd6169c99850
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-10-31 23:31:05 +0800

    Updated database intialization

---
+++ WebApp/Database Script/Database_Initialization.sql
@@ -0,0 +1,157 @@
+INSERT INTO NARRATIVE VALUES
+(1, 'In 2000, SMU commenced its curriculum with the School of Business, welcoming its first batch of cohort in August. The SMU School of Business and university-wide scholars programme was named in perpetuity after Dr Lee Kong Chian in recognition of the Lee Foundation generous contribution of S$50 million to SMU in 2004. Dr Lee Kong Chain is a well-known Southeast Asia businessmen, philanthropist and community leader. SMU school of business is therefore known as LKCSB (Lee Kong Chian School of Business)'),
+(2, 'School of Accountancy (SOA) is the second largest school in SMU, with its humble beginnings going way back to 2000. The first Dean then was Professor Pang Yang Hoong, who was also part of the planning team at SMU. A lecture hall lies within SOA, being the only school to have a lecture hall. It was set up by the Chinese immigrants in the late 19th century, naming it after the charitable foundation Ngee Ann Kongsi - Ngee Ann Kongsi Auditorium.'),
+(3, 'The School of Economics and Social Science was established on 1 July 2002 under the Bachelor of Science degree. The Bachelor of Social Science undergraduate degree programme had 3 majors -Psychology, Sociology, and Political Science. In April 2007, the School of Economics and the School of Social Sciences were established as separate schools to better focus on the development of the undergraduate and graduate curricula and programmes, as well as faculty research in the respective disciplines. The current dean for School of Economics and School of Social Science are Prof Bryce Hool and Tan Yoo Guan respectively.'),
+(4,'Established in 2003, School of Information Systems is a dynamic school that is constantly seeking to lift our education and research to a higher level. It houses 10 institutes, centres, labs and initiatives (ICLIs) in SMU. These ICLIs have several areas of focus such as Analytics for Business, Consumer & Social Insights, Urban Management & Sustainability, Ageing & Healthcare Management, Finance & Financial Markets, and Cybersecurity Initiative.'),
+(5,'School of law officially commence in 2007, welcoming its first batch of student to its LLB and JD programmes at the helm of Professor Michael Furmston. However, the law department has always been in SMU within Lee Kong Chian School of Business, created and headed by Professor Andrew Phang (presently Judge of Appeal). The SOL Building broke ground on 20 January 2014 and was officially opened in 2017. Prior to this, School of Law was sharing the same building as School of Accountancy. A key feature of the new building is the Kwa Geok Choo Law Library, named in memory of the late Madam Kwa Geok Choo, wife of former Prime Minister Lee Kuan Yew. The 2,200-square-metre Law Library is fully equipped with modern technology wired for legal research in the 21st century.'),
+(6,'Officially opened on 24 February 2006, the library was named after Hong Kong businessman Dr. Li Ka-Shing, Chairman of Cheung Kong (Holdings) Limited and Hutchison Whampoa Limited. The Li Ka Shing Foundation donated an endowment to the library for collections and to the University for scholarship. SMU Libraries offers a range of learning spaces, including open areas for individual and collaborative use, study carrels, project rooms equipped with LCD panels, learning labs, quiet areas, investment studio, graduate lounges, and Hive - an innovative space for collaborative learning.'),
+(7, 'Home to around 10,000 undergraduates and postgraduates, SMU comprises six schools which offer a wide range of bachelor’s, master’s and PhD degree programmes in the disciplinary areas associated with the six schools, as well as in inter- disciplinary combinations of these areas. At level three, there is a Multi-Purpose Sports Hall, commonly used for CCA training sessions, competition, camps, examination hall and many more.');
+
+INSERT INTO AR_GAME VALUES (1,'1');
+
+INSERT INTO HOTSPOT VALUES
+('Lee Kong Chian School of Business', '1.2953', '103.8506', 1, 1),
+('School of Accountancy', '1.2956', '103.8498', 2, 1),
+('School of Economics/School of Social Sciences', '1.2979', '103.8489', 3, 1),
+('School of Information Systems', '1.2974', '103.8495', 4, 1),
+('School of Law', '1.2949', '103.8495', 5, 1),
+('Li Ka Shing Library', '1.2962', '103.8501', 6, 1),
+('Administrative Building', '1.268', '103.8522', 7, 1);
+
+INSERT INTO MISSION VALUES 
+(1, 'Lee Kong Chian School of Business'),
+(2, 'School of Accountancy'),
+(3, 'School of Economics/School of Social Sciences'),
+(4, 'School of Information Systems'),
+(5, 'Li Ka Shing Library'),
+(6, 'Administrative Building');
+
+INSERT INTO QUIZ_QUESTION
+('In the CIRCLE values, what does the first alphabet \'C\' represent?', 'Commitment', 1),
+('In the CIRCLE values, what does the alphabet \'I\' represent?', 'Integrity', 1),
+('In the CIRCLE values, what does the alphabet \'R\' represent?', 'Responsibility', 1);
+('In the CIRCLE values, what does the fourth alphabet \'C\' represent?', 'Collegiality', 1),
+('In the CIRCLE values, what does the alphabet \'L\' represent?', 'Leadership', 1),
+('In the CIRCLE values, what does the alphabet \'E\' represent?', 'Excellence', 1),
+('A game changer represents:', 'Transformative Education for A New Generation of Graduates', 2),
+('A Great University represents:', 'Tacking World’s Complexities, Impacting Human Positively', 2),
+('A game catalyst represents:', 'Leader In Cutting Edge Multi-disciplinary Research', 2),
+('A Global Exemplar represents:', 'Pre-eminent and Esteemed Global City University in Asia', 2),
+('Who is the founding dean of School of Economics?', 'Professor Roberto S. Mariano', 3),
+('What is one way in which SMU supports staff in achieving excellence?', 'Open Enrolment Program', 3),
+('What does collegiality entail in SMU?', 'Being a responsible citizen in the University Community', 3),
+('Dean Prof Pang Hwee Hwa is the ______ Dean of School of Information Systems.', 'second', 4),
+('All proceeds from B3 Burger Beer Bistro at the Basement Concourse go to ________.', 'foundation of SMU guild house in the not too distant future.', 4),
+('The Living Analytics Research Centre (LARC) is a collaboration with ________.', 'Carnegie Mellon University', 4),
+('Which level is the Office of Finance located at?', '11', 7),
+('We are required to tap the employee card in the lift’s card reader if we want to access Levels __  and above.', '7', 7),
+('The University Lounge is a great place for all employees and alumni members to relax. It is located at Level ___ in the Administration building.', '6', 7),
+
+
+
+INSERT INTO QUIZ_OPTION
+('In the CIRCLE values, what does the first alphabet \'C\' represent?', 'Commitment'),
+('In the CIRCLE values, what does the first alphabet \'C\' represent?', 'Character'),
+('In the CIRCLE values, what does the first alphabet \'C\' represent?', 'Culture'),
+('In the CIRCLE values, what does the first alphabet \'C\' represent?', 'Courage'),
+('In the CIRCLE values, what does the alphabet \'I\' represent?', 'Integrity'),
+('In the CIRCLE values, what does the alphabet \'I\' represent?', 'Intelligent'),
+('In the CIRCLE values, what does the alphabet \'I\' represent?', 'Inspiring'),
+('In the CIRCLE values, what does the alphabet \'I\' represent?', 'Innovative'),
+('In the CIRCLE values, what does the alphabet \'R\' represent?', 'Responsibility'),
+('In the CIRCLE values, what does the alphabet \'R\' represent?', 'Rational'),
+('In the CIRCLE values, what does the alphabet \'R\' represent?', 'Respect'),
+('In the CIRCLE values, what does the alphabet \'R\' represent?', 'Resolved'),
+('In the CIRCLE values, what does the fourth alphabet \'C\' represent?', 'Collegiality'),
+('In the CIRCLE values, what does the fourth alphabet \'C\' represent?', 'Character'),
+('In the CIRCLE values, what does the fourth alphabet \'C\' represent?', 'Culture'),
+('In the CIRCLE values, what does the fourth alphabet \'C\' represent?', 'Courage'),
+('In the CIRCLE values, what does the alphabet \'L\' represent?', 'Leadership'),
+('In the CIRCLE values, what does the alphabet \'L\' represent?', 'Lively'),
+('In the CIRCLE values, what does the alphabet \'L\' represent?', 'Linguistic'),
+('In the CIRCLE values, what does the alphabet \'L\' represent?', 'Loyal'),
+('In the CIRCLE values, what does the alphabet \'E\' represent?', 'Excellence'),
+('In the CIRCLE values, what does the alphabet \'E\' represent?', 'Encourage'),
+('In the CIRCLE values, what does the alphabet \'E\' represent?', 'Engaging'),
+('In the CIRCLE values, what does the alphabet \'E\' represent?', 'Equal'),
+('A game changer represents:', 'Transformative Education for A New Generation of Graduates'),
+('A game changer represents:', 'Leader In Cutting Edge Multi-disciplinary Research'),
+('A game changer represents:', 'Pre-eminent and Esteemed Global City University in Asia'),
+('A game changer represents:', 'Tacking World’s Complexities, Impacting Human Positively'),
+('A game catalyst represents:', 'Tacking World’s Complexities, Impacting Human Positively'),
+('A game catalyst represents:', 'Transformative Education for A New Generation of Graduates'),
+('A game catalyst represents:', 'Leader In Cutting Edge Multi-disciplinary Research'),
+('A game catalyst represents:', 'Pre-eminent and Esteemed Global City University in Asia'),
+('A Global Exemplar represents:', 'Pre-eminent and Esteemed Global City University in Asia'),
+('A Global Exemplar represents:', 'Transformative Education for A New Generation of Graduates'),
+('A Global Exemplar represents:', 'Leader In Cutting Edge Multi-disciplinary Research'),
+('A Global Exemplar represents:', 'Pre-eminent and Esteemed Global City University in Asia'),
+('A Great University represents:', 'Pre-eminent and Esteemed Global City University in Asia'),
+('A Great University represents:', 'Transformative Education for A New Generation of Graduates'),
+('A Great University represents:', 'Leader In Cutting Edge Multi-disciplinary Research'),
+('A Great University represents:', 'Pre-eminent and Esteemed Global City University in Asia'),
+('Who is the founding dean of School of Economics?', 'Professor Roberto S. Mariano'),
+('Who is the founding dean of School of Economics?', 'Professor James Tang'),
+('Who is the founding dean of School of Economics?', 'Professor Bryce Hool'),
+('What is one way in which SMU supports staff in achieving excellence?', 'Virtual classrooms')
+('What is one way in which SMU supports staff in achieving excellence?', 'Recreational programs'),
+('What is one way in which SMU supports staff in achieving excellence?', 'Open Enrolment Program'),
+('What does collegiality entail in SMU?', 'Working together with my colleagues'),
+('What does collegiality entail in SMU?', 'Being a responsible citizen in the University Community'),
+('What does collegiality entail in SMU?', 'Developing good working relationships'),
+('Dean Prof Pang Hwee Hwa is the ______ Dean of School of Information Systems.', 'first'),
+('Dean Prof Pang Hwee Hwa is the ______ Dean of School of Information Systems.', 'second'),
+('Dean Prof Pang Hwee Hwa is the ______ Dean of School of Information Systems.', 'third'),
+('Dean Prof Pang Hwee Hwa is the ______ Dean of School of Information Systems.', 'fourth'),
+('All proceeds from B3 Burger Beer Bistro at the Basement Concourse go to ________.', 'foundation of SMU guild house in the not too distant future.'),
+('All proceeds from B3 Burger Beer Bistro at the Basement Concourse go to ________.', 'funding of scholarships for students.'),
+('All proceeds from B3 Burger Beer Bistro at the Basement Concourse go to ________.', 'B3’s owners who are not affiliated with SMU.'),
+('All proceeds from B3 Burger Beer Bistro at the Basement Concourse go to ________.', 'SMU’s building funds.'),
+('The Living Analytics Research Centre (LARC) is a collaboration with ________.', 'Harvard University'),
+('The Living Analytics Research Centre (LARC) is a collaboration with ________.', 'Oxford University'),
+('The Living Analytics Research Centre (LARC) is a collaboration with ________.', 'University of Washington'),
+('The Living Analytics Research Centre (LARC) is a collaboration with ________.', 'Carnegie Mellon University'),
+('Which level is the Office of Finance located at?', '10'),
+('Which level is the Office of Finance located at?', '11'),
+('Which level is the Office of Finance located at?', '12'),
+('Which level is the Office of Finance located at?', '13'),
+('We are required to tap the employee card in the lift’s card reader if we want to access Levels __  and above.', '7'),
+('We are required to tap the employee card in the lift’s card reader if we want to access Levels __  and above.', '8'),
+('We are required to tap the employee card in the lift’s card reader if we want to access Levels __  and above.', '9'),
+('We are required to tap the employee card in the lift’s card reader if we want to access Levels __  and above.', '10'),
+('The University Lounge is a great place for all employees and alumni members to relax. It is located at Level ___ in the Administration building.', '6'),
+('The University Lounge is a great place for all employees and alumni members to relax. It is located at Level ___ in the Administration building.', '5'),
+('The University Lounge is a great place for all employees and alumni members to relax. It is located at Level ___ in the Administration building.', '4'),
+('The University Lounge is a great place for all employees and alumni members to relax. It is located at Level ___ in the Administration building.', '3'),
+
+
+
+INSERT INTO SUBMISSION_QUESTION VALUES
+(1, 'Find the Mission Statement and take a wefie!', 1),
+(2, 'Find the Mission Statement and take a wefie!', 2),
+(3, 'Find Le Suantio Gallery and take a wefie!', 3),
+(4, 'Locate the SMU Gym and take a wefie together with one qualified student fitness instructor!', 4),
+(5, 'Locate Living Analytics Research Centre (LARC) and take a wefie together with LARC’s logo!', 4),
+(6, 'Locate the SMU Hall and take a wefie outside of the Hall! (Hint: Basement)', 5),
+(7, 'Locate the Kwa Geok Choo Library and take a wefie next to the scenic elevator!', 5),
+(8, 'Locate the Eagle’s Nest and take a wefie! (Hint: Its outdoors)', 5),
+(9, 'Locate the Hive at the Learning Commons and take a wefie!', 6),
+(10, 'Locate the Daringly Different book in the Library and take a wefie together with a Library staff.', 6),
+(11, 'Locate the SMU Shop in this building and take a wefie with one of the retail staff!', 6),
+(12, 'Locate the Campus Green and take a wefie together with Bernar Venet’s Sculpture “97.5° Arc x 8”!', 6),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
