cc5c3784e8d89f67f546fcd257c3609407d86a17
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-11-01 09:33:34 +0800

    Update WebApp/routes/quiz.js

--- WebApp/routes/quiz.js
+++ WebApp/routes/quiz.js
@@ -43,9 +43,9 @@ router.get('/getQuizzes', function(req, res){
                                 }
                                 count = count + 1;
 
-                                console.log('option: ' + detail.QUIZ_OPTION);
-                                console.log('currentQuiz_ID: ' + currentQuiz_ID);
-                                console.log('quiz id: ' + detail.QUIZ_ID);
+                                // console.log('option: ' + detail.QUIZ_OPTION);
+                                // console.log('currentQuiz_ID: ' + currentQuiz_ID);
+                                // console.log('quiz id: ' + detail.QUIZ_ID);
 
                                 if (currentQuiz_ID != detail.QUIZ_ID){
                                     currentQuiz_ID = detail.QUIZ_ID
