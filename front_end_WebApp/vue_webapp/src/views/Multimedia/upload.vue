d846d725a0ccf38f9164de4872a59374692f42d7
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-11-18 00:06:03 +0800

    Update front_end_WebApp/vue_webapp/src/views/Multimedia/upload.vue

--- front_end_WebApp/vue_webapp/src/views/Multimedia/upload.vue
+++ front_end_WebApp/vue_webapp/src/views/Multimedia/upload.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        Input
         <br>
-        <input type="file" id='file' ref='file' v-on:change="onFileUpload()"/>
+        <div class="upload-btn-wrapper">
+            <input type="file" id='file' ref='file' v-on:change="onFileUpload()"/>
+            <br>
+            <br>
+            <button class="btn">Add File Here</button>
+        </div>
         <br>
-        <br>
-        <button v-on:click="onUpload()">Upload</button>
+        <button class="submitbtn" v-on:click="onUpload()">Upload</button>
     </div>
 </template>
 
@@ -43,3 +46,40 @@ export default{
 }
     
 </script>
+
+<style>
+.upload-btn-wrapper {
+  position: relative;
+  overflow: hidden;
+  display: inline-block;
+}
+
+.btn {
+  border: 2px solid gray;
+  color: gray;
+  background-color: white;
+  padding: 8px 20px;
+  border-radius: 8px;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.upload-btn-wrapper input[type=file] {
+  font-size: 100px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 0;
+}
+
+.submitbtn{
+    background: rgb(1, 98, 188);
+    color: #fff;
+    border: 1px solid #eee;
+    border-radius: 20px;
+    box-shadow: 5px 5px 5px #eee;
+    text-shadow:none;
+    font-size: 130%;
+    margin-top: 30px;
+}
+</style>
