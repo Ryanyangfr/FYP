f7462aa9967723799228da4055faaa373f09c854
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-12-12 21:52:49 +0800

    Update draganddrop.js

--- WebApp/routes/draganddrop.js
+++ WebApp/routes/draganddrop.js
@@ -42,6 +42,7 @@ router.get('/getDragAndDrop',function(req,res){
                             response.push({hotspot: hotspot_name, drag_and_drop: drag_and_drop_});
                             console.log('count: ' + count);
                             console.log('mission length: ' + missions.length);
+                            console.log(count==mission.length)
                             if(count == mission.length){
                                 res.end(JSON.stringify(response, null, 3));
                                 console.log('count: ' + count);
