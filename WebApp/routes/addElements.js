e3acdba343cb0e54fc0625b731b0322e45e6b3e9
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-12-14 16:55:38 +0800

    Create addElements.js

---
+++ WebApp/routes/addElements.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+var databaseConfig = require('../config/mysqlconf.js');
+
+var conn = mysql.createConnection(databaseConfig);
+
+router.post('/addHotspot', function(req,res){
+    var hotspotName = req.body.hotspot_name;
+    var latitude = req.body.latitude;
+    var longtitude = req.body.longtitude;
+    var narrative_id = req.body.narrative_id;
+
+    var query = 'INSERT INTO HOTSPOT VALUES (?,?,?,?)';
+
+    conn.query(query,[hotspotName,latitude,longtitude,narrative_id], function(err, query){
+        if(err){
+            console.log(err);
+        }else{
+            res.end({success: true});
+        }
+    })
+})
+
+
+
