a1e0dff840a4dd57e6838ee57c6907b1052d4f4a
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-12-14 16:09:35 +0800

    Update narratives.js

--- WebApp/routes/narratives.js
+++ WebApp/routes/narratives.js
@@ -8,7 +8,22 @@ var databaseConfig = require('../config/mysqlconf.js');
 var conn = mysql.createConnection(databaseConfig);
 
 router.get('/getNarratives', function(req,res){
-    var query = 'SELECT * FROM NARRATIVE'
+    var query = 'SELECT * FROM NARRATIVE';
+    var response = [];
+
+    conn.query(query, function(narratives, err){
+        if(err){
+            console.log(err)
+        } else{
+            narratives.forEach(function(narrative){
+                var title = narrative.NARRATIVE_TITLE;
+                var id = narrative.NARRATIVE_ID;
+
+                response.push({narrative_title: title, narrative_id: id})
+            })
+            res.send(response)
+        }
+    })
 })
 
 module.exports = router;
