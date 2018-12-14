aedef70b50129ebe69234563fb9fc8d635639618
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2018-12-14 18:30:59 +0800

    Update addElements.js

--- WebApp/routes/addElements.js
+++ WebApp/routes/addElements.js
@@ -24,7 +24,7 @@ router.post('/addHotspot', function(req,res){
         if(err){
             console.log(err);
         }else{
-            res.end({success: true});
+            res.send({success: true});
         }
     })
 })
