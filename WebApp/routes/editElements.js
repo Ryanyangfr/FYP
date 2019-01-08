720b7e202bf05c8e4dc10f4d1b3186d4e356d977
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-08 12:48:46 +0800

    Update editElements.js

--- WebApp/routes/editElements.js
+++ WebApp/routes/editElements.js
@@ -11,7 +11,7 @@ router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 router.post('/editHotspot', function(req,res){
-    var hotspotName = req.body.hotspot_name;
+    var hotspotName = req.body.hotspot_name.value;
     var latitude = req.body.latitude;
     var longtitude = req.body.longtitude;
     var narrative_id = req.body.narrative_id;
@@ -20,7 +20,6 @@ router.post('/editHotspot', function(req,res){
     var query = 'UPDATE HOTSPOT SET LATITUDE = ?, LONGTITUDE = ?, NARRATIVE_ID = ? WHERE HOTSPOT_NAME = ?';
 
     conn.query(query,[latitude,longtitude,narrative_id,hotspotName], function(err, data){
-        console.log(data.query)
         if(err){
             console.log(err);
             res.send(JSON.stringify({success: "false"}))
