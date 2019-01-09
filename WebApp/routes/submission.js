fa37cd55ed764ec809e8746e097d1ef780db75df
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-09 14:04:08 +0800

    Update submission.js

--- WebApp/routes/submission.js
+++ WebApp/routes/submission.js
@@ -294,7 +294,7 @@ router.get('/getAllSubmissionURL', function(req,res){
     console.log('team: ' + team);
     console.log('instance_id: ' + instance_id);
 
-    var query = 'SELECT SUBMISSION_IMAGE_URL, QUESTION, HOTSPOT_NAME FROM SUBMISSION AS S, SUBMISSION_QUESTION AS SQ, MISSION AS M WHERE TEAM_ID = ? AND TRAIL_INSTANCE_ID = ? AND S.QUESTION_ID = SQ.QUESTION_ID AND M.MISSION_ID = SQ.MISSION_ID';
+    var query = 'SELECT SUBMISSION_IMAGE_URL, QUESTION, HOTSPOT_NAME FROM SUBMISSION AS S, SUBMISSION_QUESTION AS SQ, MISSION AS M WHERE TEAM_ID = ? AND TRAIL_INSTANCE_ID = ? AND S.SUBMISSION_QUESTION_ID = SQ.QUESTION_ID AND M.MISSION_ID = SQ.MISSION_ID';
 
     conn.query(query, [team, instance_id], function(err, rows){
         if (err){
@@ -305,7 +305,7 @@ router.get('/getAllSubmissionURL', function(req,res){
                 response.push({SubmissionURL: row.SUBMISSION_IMAGE_URL, hotspot: row.HOTSPOT_NAME, question: row.QUESTION});
             });
 
-            var drawing_query = 'SELECT SUBMISSION_IMAGE_URL, DRAWING_QUESTION, HOTSPOT_NAME FROM SUBMISSION AS S, DRAWING_QUESTION AS DQ, MISSION AS M WHERE TEAM_ID = ? AND TRAIL_INSTANCE_ID = ? AND S.QUESTION_ID = DQ.QUESTION_ID AND M.MISSION_ID = DQ.MISSION_ID';
+            var drawing_query = 'SELECT SUBMISSION_IMAGE_URL, QUESTION, HOTSPOT_NAME FROM SUBMISSION AS S, DRAWING_QUESTION AS DQ, MISSION AS M WHERE TEAM_ID = ? AND TRAIL_INSTANCE_ID = ? AND S.DRAWING_QUESTION_ID = DQ.QUESTION_ID AND M.MISSION_ID = DQ.MISSION_ID';
 
             conn.query(query, [team, instance_id], function(err, rows){
                 if (err){
