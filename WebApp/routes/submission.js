b43b937850831f9c1dcada97069990f526d2562d
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-11 17:28:12 +0800

    Update submission.js

--- WebApp/routes/submission.js
+++ WebApp/routes/submission.js
@@ -347,6 +347,7 @@ router.get('/getAllSubmissionURL', function(req,res){
 router.get('/getSubmission', function(req,res){
     var image_url = req.query.url;
     var parentDir = path.normalize(__dirname + "/..");
+    console.log('image_url: ' + image_url)
     res.sendFile(path.normalize(parentDir + '/' + image_url));
 })
 
