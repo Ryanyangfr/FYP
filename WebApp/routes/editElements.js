abd0684c908d0f744b943c2ba1d1bd974cae2060
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-14 12:37:37 +0800

    Update editElements.js

--- WebApp/routes/editElements.js
+++ WebApp/routes/editElements.js
@@ -22,16 +22,16 @@ router.post('/editHotspot', function(req,res){
     conn.query(query,[latitude,longtitude,narrative_id,hotspotName], function(err, data){
         if(err){
             console.log(err);
-            res.send(JSON.stringify({success: "false"}))
+            res.send(JSON.stringify({success: "false"}));
         }else{
             res.send(JSON.stringify({success: "true"}));
         }
-    })
-})
+    });
+});
 
 router.post('/editNarrative', function(req,res){
     var narrative_id = req.body.narrative_id;
-    var narrative = req.body.narrative
+    var narrative = req.body.narrative;
 
     console.log(req.body);
     var query = 'UPDATE NARRATIVE SET NARRATIVE = ? WHERE NARRATIVE_ID = ?';
@@ -39,11 +39,100 @@ router.post('/editNarrative', function(req,res){
     conn.query(query,[narrative, narrative_id], function(err, data){
         if(err){
             console.log(err);
-            res.send(JSON.stringify({success: "false"}))
+            res.send(JSON.stringify({success: "false"}));
         }else{
             res.send(JSON.stringify({success: "true"}));
         }
-    })
-})
+    });
+});
 
+router.post('/editQuiz', function(req,res){
+    var quizID = req.body.question.value;
+    var question = req.body.question.label;
+
+    var option1 = req.body.option1.option;
+    var option1ID = req.body.option1.id;
+
+    var option2 = req.body.option2.option;
+    var option2ID = req.body.option2.id;
+
+    var option3 = req.body.option3.option;
+    var option3ID = req.body.option3.id;
+
+    var option4 = req.body.option4.option;
+    var option4ID = req.body.option4.id;
+
+    updateQnQuery = 'UPDATE QUIZ SET QUIZ_QUESTION = ? WHERE QUIZ_ID = ?'
+
+    var count = 0;
+    var anyErr = false;
+
+    conn.query(updateQnQuery, [question,quizID], function(err,result){
+        if (err){
+            console.log(err);
+            res.send(JSON.stringify({success: "false"}));
+            anyErr = true;
+        } else{
+            updateOptionQuery = 'UPDATE QUIZ_OPTION SET QUIZ_OPTION = ? WHERE QUIZ_OPTION_ID = ? AND QUIZ_ID = ?'
+            
+            conn.query(updateOptionQuery, [option1,option1ID,quizID], function(err, data){
+                if (err){
+                    console.log(err);
+                    res.send(JSON.stringify({success: "false"}));
+                    anyErr = true;
+                    count += 1;
+                }else {
+                    count += 1;
+                    if(!anyErr && count == 4){
+                        res.send(JSON.stringify({success: "true"}));
+                    }
+                }
+            });
+
+            conn.query(updateOptionQuery, [option2,option2ID,quizID], function(err, data){
+                if (err){
+                    console.log(err);
+                    res.send(JSON.stringify({success: "false"}));
+                    anyErr = true;
+                    count += 1;
+                }else {
+                    count += 1;
+                    if(!anyErr && count == 4){
+                        res.send(JSON.stringify({success: "true"}));
+                    }
+                }
+            });
+
+            conn.query(updateOptionQuery, [option3,option3ID,quizID], function(err, data){
+                if (err){
+                    console.log(err);
+                    res.send(JSON.stringify({success: "false"}));
+                    anyErr = true;
+                    count += 1;
+                }else {
+                    count += 1;
+                    if(!anyErr && count == 4){
+                        res.send(JSON.stringify({success: "true"}));
+                    }
+                }
+            });
+
+            conn.query(updateOptionQuery, [option4,option4ID,quizID], function(err, data){
+                if (err){
+                    console.log(err);
+                    res.send(JSON.stringify({success: "false"}));
+                    anyErr = true;
+                    count += 1;
+                }else {
+                    count += 1;
+                    if(!anyErr && count == 4){
+                        res.send(JSON.stringify({success: "true"}));
+                    }
+                }
+            });
+        }
+    });
+
+
+});
 module.exports = router;
