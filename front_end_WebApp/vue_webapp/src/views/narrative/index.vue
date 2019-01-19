73e2e1237101a3fdbd6db67bd551022ef10cc706
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-19 13:51:00 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/Ryanyangfr/FYP""
    This reverts commit 98e4afc46b01757782545c84f7f69f1fe34b5670.

--- front_end_WebApp/vue_webapp/src/views/narrative/index.vue
+++ front_end_WebApp/vue_webapp/src/views/narrative/index.vue
@@ -3,29 +3,98 @@
         <div class="card">
             <div class="card-title">
                 <h5>Narrative List</h5>
-                <button class="create-narrative-btn"><i class="ti-plus"></i>ADD NEW</button>
+                <button class="create-narrative-btn" @click="addNarrative()"><i class="ti-plus"></i>ADD NEW</button>
             </div>
             <table>
                 <tr class="narrative-table-header">
                     <td class="narrative-title-header">Narrative Title</td>
                     <td>Narrative Description</td>
-                    <td></td>
-                    <td></td>
+                    <td colspan="2">Actions</td>
                 </tr>
 
                 <tr class = "narrative-data" v-for="narrative in narrativeTableList" :key="narrative.id">
                     <td>{{narrative.title}}</td>
                     <td>{{narrative.narrative}}</td>
-                    <td><button><i class="ti-pencil-alt"></i></button></td>
-                    <td><button><i class="ti-trash"></i></button></td>
+                    <td><button @click="editNarrative(narrative.id, narrative.title, narrative.narrative)"><i class="ti-pencil-alt"></i></button></td>
+                    <td><button @click="deleteNarrative(narrative.id,narrative.title)"><i class="ti-trash"></i></button></td>
                 </tr>
                 
             </table>
+            {{this.curr_narrative_id}}
         </div>
         
-        </br>
+        <div class="black-blur-bg" v-if="showEdit"> 
+            <div class="edit-narrative-form">
+                <div class="edit-narrative-header">
+                    <h5>Edit Narrative</h5>
+                    <button class="close-edit-narrative" @click="closeEdit()"><font-awesome-icon icon="times"/></button>
+                </div>
+                <hr>
+                
+                <form class="edit-narrative-body" @submit.prevent="onSubmitToEdit">
+                    <div class="edit-narrative-input">
+                        <input type="text" id="edit-narrative-title-input" required v-model="curr_narrative_title">
+                        <label for="edit-narrative-title-input">Narrative Title</label>
+                    </div>
+                    <div class="edit-narrative-input">
+                        <textarea id="edit-narrative-input" rows="5" cols="65" required v-model= "curr_narrative"></textarea>
+                        <label for="edit-narrative-input">Enter Narrative</label>
+                    </div>
+                    <div>
+                     <button type="submit" class="edit-narrative-submit">Submit</button>
+                    </div>
+                </form>
+               
+            </div>
+        </div>
 
-        <v-select :options="functionsAvailable" v-model="func" placeholder="Add" style="width:200px;"></v-select>
+        <div class="black-blur-bg" v-if="showAdd"> 
+            <div class="add-narrative-form">
+                <div class="add-narrative-header">
+                    <h5>New Narrative</h5>
+                    <button class="close-add-narrative" @click="closeAdd()"><font-awesome-icon icon="times"/></button>
+                </div>
+                <hr>
+                
+                <form class="add-narrative-body" @submit.prevent="onSubmitToAdd">
+                    <div class="add-narrative-input">
+                        <input type="text" id="add-narrative-title-input" v-model="curr_narrative_title" required>
+                        <label for="add-narrative-title-input">Narrative Title</label>
+                    </div>
+                    <div class="add-narrative-input">
+                        <textarea id="add-narrative-input" rows="5" cols="65" v-model="curr_narrative" required></textarea>
+                        <label for="add-narrative-input">Enter Narrative</label>
+                    </div>
+                    <div>
+                     <button type="submit" class="add-narrative-submit">Submit</button>
+                    </div>
+                </form>
+               
+            </div>
+        </div>
+
+        <!--delete narrative popup -->
+        <div class="black-blur-bg" v-if="showDelete"> 
+            <div class="delete-narrative-popup">
+                <div class="delete-narrative-header">
+                    <h5>Delete</h5>
+                    <button class="close-delete-narrative" @click="closeDelete()"><font-awesome-icon icon="times"/></button>
+                </div>
+                <hr>
+                
+                <form class="delete-narrative-body" @submit.prevent="onSubmitToDelete">
+                    <div><h6>Are you sure you want to delete "{{this.curr_narrative_title}}"?</h6></div>
+                    <div><hr></div>
+                    <div class="delete-narrative-btm">
+                        <button class="cancel-delete" @click="closeDelete()">Cancel</button>
+                        <button type="submit" class="delete-narrative-btn">Delete</button>
+                    </div>
+                </form>
+               
+            </div>
+        </div>
+
+        <!--<v-select :options="functionsAvailable" v-model="func" placeholder="Add" style="width:200px;"></v-select>
 
         <form @submit.prevent="onSubmitToAdd" v-if="func == functionsAvailable[0]">
             Narrative Title:
@@ -46,7 +115,8 @@
         <form @submit.prevent="onSubmitToDelete" v-if="func == functionsAvailable[2]">
             <v-select :options="dropDownList" v-model="narrativeToBeDeleted" placeholder="Please select a narrative" style="width:200px;"></v-select>
              <button type="submit">Delete</button>
-        </form>
+        </form>-->
+
         <!-- {{this.title}}
         {{this.narrative}} -->
         <!-- {{narrative}} -->
@@ -60,6 +130,9 @@ export default {
     name: "narrative",
     data() {
         return{
+            showEdit: false,
+            showAdd:false,
+            showDelete:false,
             func: "Add",
             functionsAvailable: ["Add", "Edit", "Delete"],
             title: "",
@@ -67,46 +140,126 @@ export default {
             narrativeToBeEdited: "",
             dropDownList: [],
             narrativeToBeDeleted: "",
-            narrativeTableList: []
+            narrativeTableList: [],
+            curr_narrative_id: "",
+            curr_narrative: "",
+            curr_narrative_title: ""
         }
     },
     components:{
         vSelect
     },
     methods: {
+
+        addNarrative(){
+            if(this.showAdd){
+                this.showAdd = false;
+            } else{
+                this.showAdd = true;
+            }
+
+        },
+
+        closeAdd(){
+            if(this.showAdd){
+                this.showAdd = false;
+            } else{
+                this.showAdd = true;
+            }
+        },
+
         onSubmitToAdd() {
             var postBody = {
-                "narrative": this.narrative,
-	            "title": this.title
+                "narrative": this.curr_narrative,
+	            "title": this.curr_narrative_title
             }
             axios.post('http://54.255.245.23:3000/add/addNarrative', postBody)
             .then(response => {
                 let data = response.data
                 console.log(data)
             })
+
+            location.reload();
         },
 
         onSubmitToEdit() {
             var postBody = {
-                "narrative_id": this.narrativeToBeEdited.value,
-	            "narrative": this.narrative
+                "narrative_id": this.curr_narrative_id,
+	            "narrative": this.curr_narrative
             }
             axios.post('http://54.255.245.23:3000/edit/editNarrative', postBody)
             .then(response => {
                 let data = response.data
                 console.log(data)
             })
+
+            if(this.showEdit){
+                this.showEdit = false;
+            } else{
+                this.showEdit = true;
+            }
+
+            location.reload();
+        },
+
+        editNarrative(narrative_id, narrative_title, narrative){
+            if(this.showEdit){
+                this.showEdit = false;
+            } else{
+                this.showEdit = true;
+            }
+
+            this.curr_narrative = narrative;
+            this.curr_narrative_title = narrative_title;
+            this.curr_narrative_id = narrative_id;
+
+        },
+
+        closeEdit(){
+            if(this.showEdit){
+                this.showEdit = false;
+            } else{
+                this.showEdit = true;
+            }
+        },
+
+        deleteNarrative(narrative_id, narrative_title){
+            this.curr_narrative_id = narrative_id;
+            this.curr_narrative_title = narrative_title;
+
+            if(this.showDelete){
+                this.showDelete = false;
+            } else{
+                this.showDelete = true;
+            }
+        },
+
+        closeDelete(){
+             if(this.showDelete){
+                this.showDelete = false;
+            } else{
+                this.showDelete = true;
+            }
         },
 
         onSubmitToDelete(){
              var postBody = {
-                "narrative_id": this.narrativeToBeDeleted.value,
+                // "narrative_id": this.narrativeToBeDeleted.value,
+                "narrative_id": this.curr_narrative_id
             }
             axios.post('http://54.255.245.23:3000/delete/deleteNarrative', postBody)
             .then(response => {
                 let data = response.data
                 console.log(data)
             })
+
+            if(this.showDelete){
+                this.showDelete = false;
+            } else{
+                this.showDelete = true;
+            }
+
+            location.reload();
         }
     },
     mounted(){
@@ -132,11 +285,14 @@ export default {
 <style>
     @import url("https://fonts.googleapis.com/css?family=Roboto+Condensed|Roboto");
     @import '../../assets/themify-icons.css';
-
+    
+    label{
+        font-family: 'lato', sans-serif
+    }
     .card{
         padding: 18px;
         margin: 18px;
-        border-radius: 5px;
+        border-radius: 3px;
         border: none;
         font-family: 'Roboto Condensed', sans-serif; 
     }
@@ -222,11 +378,218 @@ export default {
 
     .narrative-title-header{
         min-width: 200px;
-       
+    }
+
+
+    /*overlay for edit starts*/
+
+    .black-blur-bg{
+        width:100%;
+        height: 100%;
+        background-color: rgb(0, 0, 0, 0.7);
+        position: fixed;
+        top:0;
+        z-index: 2;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+    }
+
+    .edit-narrative-form, .add-narrative-form{
+        width:35%;
+        height:65%;
+        background:white;
+        opacity: 100%;
+        z-index: 500;
+        border-radius: 3px;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 600;
         
     }
-</style>
 
+    .edit-narrative-header, .add-narrative-header, .delete-narrative-header{
+        max-width: 100%;
+        padding:18px;
+    }
+
+    .edit-narrative-form h5, .add-narrative-form h5, .delete-narrative-popup h5{
+        display: flex;
+        float: left;
+    }
+
+    .close-edit-narrative, .close-add-narrative, .close-delete-narrative{
+        background: none;
+        border: none;
+        color: #868686;
+        cursor: pointer;
+        float: right;
+        font-size: 18px;
+    }
+
+    .edit-narrative-input, .add-narrative-input{
+        float: left;
+        display: flex;
+        margin-left: 18px;
+        margin-bottom: 45px;
+        font-family: 'Lato', sans-serif;
+        position: relative;
+    }
+
+    .edit-narrative-body, .add-narrative-body{
+        padding-top: 25px;
+        display: flex;
+        flex-direction: column;
+        width:100%;
+    }
+
+    .edit-narrative-input label, .add-narrative-input label{
+        top: -25px;
+        position: absolute;
+        font-size: 13px;
+        pointer-events: none;
+        transition: all 0.3s ease 0s;
+    }
+
+    .edit-narrative-input input:focus ~ label,
+    .edit-narrative-input input:valid ~ label,
+    .edit-narrative-input input:-webkit-autofill + label,
+    .add-narrative-input input:focus ~ label,
+    .add-narrative-input input:valid ~ label,
+    .add-narrative-input input:-webkit-autofill + label{
+        font-size: 14px
+    }
+
+    .edit-narrative-input input, .add-narrative-input input{
+        margin-left: 5px;
+        height: 40px;
+        outline: none;
+        border: 1px solid #CED4DA;
+        border-radius: 4px;
+        padding: 10px;
+        font-size: 14px;
+        width:90%
+    }
+
+    .edit-narrative-input input:focus, .add-narrative-input input:focus{
+        outline: none !important;
+        border:1px solid #6200EE;
+        box-shadow: 0 0 2px #645cdd;
+    }
+
+    .edit-narrative-input textarea:focus, .add-narrative-input textarea:focus{
+        outline: none !important;
+        border:1px solid #6200EE;
+        box-shadow: 0 0 2px #645cdd;
+    }
+
+    .edit-narrative-input textarea, .add-narrative-input textarea{
+        resize: none;
+        margin-left: 5px;
+        outline: none;
+        border: 1px solid #CED4DA;
+        border-radius: 4px;
+        padding: 10px;
+        font-size: 14px;
+        width:90%;
+    }
+
+    .edit-narrative-submit, .add-narrative-submit{
+        background-color: #6200EE;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        font-size:15px;
+        display: flex;
+        float: right;
+        padding:10px 20px 10px 20px;
+        margin-right: 25px;
+        margin-bottom: 25px;
+        text-align: center;
+        cursor: pointer;
+        align-items: center;
+        position: relative;
+        font-family: "Roboto", sans-serif
+        
+    }
+
+ /*overlay for edit ends*/
+
+    .delete-narrative-popup{
+        min-width: 30%;
+        min-height: 33%;
+        background-color: white;
+        opacity: 100%;
+        z-index: 500;
+        border-radius: 3px;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 600;
+        overflow: hidden;
+    }
+    
+    .delete-narrative-body{
+        width:100%;
+        height: 130px;
+        overflow: hidden;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        padding-top: 10px;
+    }
+
+    .delete-narrative-body h6{
+        display: flex;
+        float: left;
+        height: 100%;
+        width: 100%;
+        font-size: 15px;
+        margin-left: 20px;
+        margin-bottom: 10px;
+    }
+
+    .delete-narrative-btm{
+        margin-bottom: 10px;
+        margin-top: 10px;
+    
+    }
+
+    .cancel-delete{
+        background: none;
+        border: 1px solid #C6C4BC;
+        border-radius: 4px;
+        display: flex;
+        float: left;
+        padding:8px 15px 8px 15px;
+        margin-left: 25px;
+        text-align: center;
+        cursor: pointer;
+        align-items: center;
+        position: relative;
+        font-family: 'Roboto', sans-serif;
+        font-size: 17px;
+        color: #666666;
+    }
+
+    .delete-narrative-btn{
+        background: none;
+        border: none;
+        background-color: #F15E5E;
+        border-radius: 4px;
+        display: flex;
+        float: right;
+        padding:8px 15px 8px 15px;
+        margin-right: 25px;
+        text-align: center;
+        cursor: pointer;
+        align-items: center;
+        position: relative;
+        font-family: 'Roboto', sans-serif;
+        font-size: 17px;
+        color: white;
+    }
+
+    
+</style>
 
 
 
