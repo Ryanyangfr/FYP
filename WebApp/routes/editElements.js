12270e912665c33d1a74dda242b8030a1282e749
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-22 18:30:56 +0800

    Update editElements.js

--- WebApp/routes/editElements.js
+++ WebApp/routes/editElements.js
@@ -11,7 +11,7 @@ router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 router.post('/editHotspot', function(req,res){
-    var hotspotName = req.body.hotspot_name.value;
+    var hotspotName = req.body.hotspot_name;
     var latitude = req.body.latitude;
     var longtitude = req.body.longtitude;
     var narrative_id = req.body.narrative_id;
