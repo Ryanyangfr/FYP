e05115b0efc18a463a8bba53d583937339a8c62b
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-23 21:50:14 +0800

    Updated editing of wefie questions and deleting of wefie questions

--- WebApp/routes/deleteElements.js
+++ WebApp/routes/deleteElements.js
@@ -1,87 +1,101 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
+const express = require('express');
 
-var databaseConfig = require('../config/mysqlconf.js');
+const router = express.Router();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-var conn = mysql.createConnection(databaseConfig);
+const databaseConfig = require('../config/mysqlconf.js');
+
+const conn = mysql.createConnection(databaseConfig);
 
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-router.post('/deleteHotspot', function(req,res){
-    var hotspotName = req.body.hotspot_name
-    
-    console.log(hotspotName);
-    var query = 'DELETE FROM HOTSPOT WHERE HOTSPOT_NAME = ?'
+router.post('/deleteHotspot', (req, res) => {
+  const hotspotName = req.body.hotspot_name;
 
-    conn.query(query, hotspotName, function(err, data){
-        if(err){
+  console.log(hotspotName);
+  const query = 'DELETE FROM HOTSPOT WHERE HOTSPOT_NAME = ?';
+
+  conn.query(query, hotspotName, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send(JSON.stringify({ success: 'false' }));
+    } else {
+      res.send(JSON.stringify({ success: 'true' }));
+    }
+  });
+});
+
+router.post('/deleteNarrative', (req, res) => {
+  const narrative_id = req.body.narrative_id;
+
+  console.log(narrative_id);
+  const query = 'DELETE FROM NARRATIVE WHERE NARRATIVE_ID = ?';
+
+  conn.query(query, narrative_id, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send(JSON.stringify({ success: 'false' }));
+    } else {
+      res.send(JSON.stringify({ success: 'true' }));
+    }
+  });
+});
+
+router.post('/deleteQuiz', (req, res) => {
+  const mission_id = req.body.mission_id;
+
+  console.log(mission_id);
+  const query = 'SELECT QUIZ_ID FROM QUIZ WHERE MISSION_ID = ?';
+
+  conn.query(query, mission_id, (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const deleteOptionsQuery = 'DELETE FROM QUIZ_OPTIONS WHERE QUIZ_ID = ?';
+      const deleteQuizQuery = 'DELETE FROM QUIZ WHERE MISSION_ID = ?';
+      const deleteMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
+
+      rows.forEach((row) => {
+        conn.query(deleteOptionsQuery, row.QUIZ_ID, (err, data) => {
+          if (err) {
             console.log(err);
-            res.send(JSON.stringify({success: "false"}));
-        }else{
-            res.send(JSON.stringify({success: "true"}));
-        }
-    })
-})
-
-router.post('/deleteNarrative', function(req,res){
-    var narrative_id = req.body.narrative_id
-    
-    console.log(narrative_id);
-    var query = 'DELETE FROM NARRATIVE WHERE NARRATIVE_ID = ?'
-
-    conn.query(query, narrative_id, function(err, data){
-        if(err){
-            console.log(err);
-            res.send(JSON.stringify({success: "false"}));
-        }else{
-            res.send(JSON.stringify({success: "true"}));
-        }
-    })
-})
-
-router.post('/deleteQuiz', function(req,res){
-    var mission_id = req.body.mission_id
-    
-    console.log(mission_id);
-    var query = 'SELECT QUIZ_ID FROM QUIZ WHERE MISSION_ID = ?';
-
-    conn.query(query, mission_id, function(err, rows){
-        if (err){
-            console.log(err);
-        } else{
-            var deleteOptionsQuery = 'DELETE FROM QUIZ_OPTIONS WHERE QUIZ_ID = ?';
-            var deleteQuizQuery = 'DELETE FROM QUIZ WHERE MISSION_ID = ?';
-            var deleteMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
-
-            rows.forEach(function(row){
-                conn.query(deleteOptionsQuery, row.QUIZ_ID, function(err,data){
-                    if(err){
-                        console.log(err);
-                        res.send(JSON.stringify({success: "false"}));
-                    } else{
-                        conn.query(deleteQuizQuery, mission_id, function(err, data2){
-                            if(err){
-                                console.log(err);
-                                res.send(JSON.stringify({success: "false"}));
-                            } else{
-                                conn.query(deleteMissionQuery, mission_id, function(err,data3){
-                                    if(err){
-                                        console.log(err);
-                                        res.send(JSON.stringify({success: "false"}));
-                                    }
-                                });
-                            }
-                        });
-                    }
+            res.send(JSON.stringify({ success: 'false' }));
+          } else {
+            conn.query(deleteQuizQuery, mission_id, (err, data2) => {
+              if (err) {
+                console.log(err);
+                res.send(JSON.stringify({ success: 'false' }));
+              } else {
+                conn.query(deleteMissionQuery, mission_id, (err, data3) => {
+                  if (err) {
+                    console.log(err);
+                    res.send(JSON.stringify({ success: 'false' }));
+                  }
                 });
+              }
             });
-            res.send(JSON.stringify({success: "true"}));
-        }
-    });
+          }
+        });
+      });
+      res.send(JSON.stringify({ success: 'true' }));
+    }
+  });
+});
+
+router.post('/deleteWefieQuestion', (req, res) => {
+  const wefieID = req.body.id;
+  const query = 'DELETE FROM SUBMISSION_QUESTION WHERE QUESTION_ID = ?';
+  
+  conn.query(query, wefieID, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send(JSON.stringify({ success: 'false' }));
+    } else {
+      res.send(JSON.stringify({ success: 'true' }));
+    }
+  });
 });
 
 module.exports = router;
-
