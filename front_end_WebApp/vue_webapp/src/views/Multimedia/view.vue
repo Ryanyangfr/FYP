8a3034248437cf032d39b37851ced8ec8e27252e
Raylene Ng Min Yun <raylene.ng.2016@smu.edu.sg>
2019-01-25 14:47:42 +0800

    updated styling and animations for submissions

--- front_end_WebApp/vue_webapp/src/views/Multimedia/view.vue
+++ front_end_WebApp/vue_webapp/src/views/Multimedia/view.vue
@@ -1,14 +1,33 @@
 <template>
     <div class="Submissions"> 
+        <form class="search-bar" @submit.prevent="retrieveAllTeams">
+            <input type="text" placeholder="Enter Trail ID" v-model="trailID" required>
+            <button class="search-btn"><i class="ti-search"></i></button> 
+        </form>
         <div class="submissions-row">
             <div class= "container" v-for="team in teamList" :key="team.team_id">
-                <button class="submission-card">
-                    Team {{team.team_id}}
+                <button class="grp-card" @click="showSubmissions(team.team_id)">
+                    <h5>Team {{team.team_id}}</h5>
+                    <br>
+                    <h6>Total: {{team.size}} Submissions</h6>
                 </button>
             </div>
         </div>
+        <div class="grp-submission-row" v-if="showSub">
+             <div class="container" v-for="(image,index) in this.images" :key="image">
+                <div class="submission-card">
+                    <div class="image-area">
+                        <img :src="image"/>  
+                    </div>
+                    <div class="submission-details">
+                        {{questions[index]}}
+                    </div> 
+                </div>
+            </div>
+            
+        </div>
      
-        <form @submit.prevent="load">
+        <!--<form @submit.prevent="load">
             Enter team:
             <input name="team" type="text" placeholder="team id" v-model="team">
             Enter trail instance id:
@@ -26,7 +45,7 @@
         {{this.team}}
         {{this.paths}}
         {{this.questions}}
-        {{this.images.length}}
+        {{this.images.length}}-->
     </div>
 </template>
 
@@ -36,20 +55,48 @@ export default{
     name: 'viewUploads',
     data() {
         return{
-            trail: '',
+            trailID: '',
+            showGrps: true,
+            showSub: false,
             team: '',
             paths: [],
             images: [],
             questions: [],
             teamList: [],
+            numSubmissionsPerTeamDict: {},
+            button:{
+                text: 'Click to view submissions'
+            }
         }
     }
     ,
     methods:{
-        retrieveAllUrl(){
+        retrieveAllTeams(){
+            this.teamList = [];
+            axios.get('http://54.255.245.23:3000/team/getAllTeams?trail_instance_id='+this.trailID)
+            .then(response => {
+                let data = response.data;
+                for(var row in data){
+                    console.log(data[row]);
+                    let team_id = data[row].team_id;
+                    axios.get('//54.255.245.23:3000/upload/getAllSubmissionURL?team='+team_id+'&trail_instance_id='+this.trailID)
+                    .then(res => {
+                        let submissionsURLs = res.data;
+                        let size = Object.keys(submissionsURLs).length;
+                        this.teamList.push({team_id: team_id, size: size}); 
+                        if(this.teamList.length === data.length){
+                            this.teamList.sort(function(a,b){ return a.team_id - b.team_id });
+                        }
+                    });
+                }
+                
+            });
+        }, 
+
+        retrieveAllUrl(teamID){
             // console.log('54.255.245.23:3000/upload/getAllSubmissionURL?team='+this.team+'&trail_instance_id='+this.trail)
-            axios.get('//54.255.245.23:3000/upload/getAllSubmissionURL?team='+this.team+'&trail_instance_id='+this.trail)
-            .then(response =>{
+            axios.get('//54.255.245.23:3000/upload/getAllSubmissionURL?team='+teamID+'&trail_instance_id='+this.trailID)
+            .then(response=>{
                 let data = response.data
                 let size = Object.keys(data).length
                 console.log(response.data)
@@ -66,14 +113,14 @@ export default{
                     this.paths.push(temp.submissionURL)
                     this.questions.push(temp.question)
                 }
-                // console.log(this.questions)
+                console.log(this.questions)
                 // console.log(response.data[0]['SubmissionURL'])
             })
         },
-        load(){
+        load(teamID){
             // this.paths = []
             // this.images = []
-            this.retrieveAllUrl();
+            this.retrieveAllUrl(teamID);
             console.log(this.paths)
             this.question = [];
             for(var index in this.paths){
@@ -103,6 +150,20 @@ export default{
                 })
             }
             // vm.$forceUpdate()
+        },
+
+        showSubmissions(teamID){
+            if(this.showSub){
+                // this.showGrps = false;
+                this.showSub = false;
+            } else{
+                // this.showGrps = true;
+                this.showSub = true;
+
+            }
+
+            this.load(teamID);
+
         }
     },
     mounted(){
@@ -110,25 +171,72 @@ export default{
             console.log("check")
             this.$router.push('/')
         }
-        
-        axios.get('http://54.255.245.23:3000/team/getAllTeams?trail_instance_id=175239')
-        .then(response => {
-            let data = response.data;
-            for(var row in data){
-                console.log(data[row])
-                this.teamList.push({team_id: data[row].team_id});
-            }
-        })
     }
 }
 </script>
 
-<style>
-    @import url("https://fonts.googleapis.com/css?family=Roboto+Condensed|Roboto");
+<style lang="scss" scoped>
+    @import url("https://fonts.googleapis.com/css?family=Roboto+Condensed|Roboto|Lato|Permanent+Marker");
     @import '../../assets/themify-icons.css';
+
+    .search-bar{
+        display: flex;
+        flex-direction: row;
+        margin: 25px 35px 25px 35px;
+        height:60px;
+        align-self: center;
+        transition: all 0.3s ease 0s;
+    }
+
+    .search-bar input{
+        flex-grow: 2;
+        background-color: white;
+        border-left: 1px solid #ededed;
+        border-top: 1px solid #ededed;
+        border-bottom: 1px solid #ededed;
+        border-right: 0;
+        border-radius: 5px 0 0 5px;
+        padding-left: 20px;
+        font-family: 'Lato', sans-serif;
+        font-size: 15px;
+        /*background-color: #ffc107;*/
+    }
+
+    .search-btn{
+        border:none;
+        padding-right: 18px;
+        background-color: white;
+        /*color:#BAB1B3;*/
+        font-size: 20px;
+        border-right: 1px solid #ededed;
+        border-top: 1px solid #ededed;
+        border-bottom: 1px solid #ededed;
+        border-radius: 0 5px 5px 0;
+        border-left: 0;
+        cursor: pointer;
+    }
+
+    .search-bar input:focus{
+        outline: none !important;
+        border-left:2px solid #645cdd;
+        border-top:2px solid #645cdd;
+        border-bottom:2px solid #645cdd;
+        border-right: 0px;
+       
+    }
+
+    .search-bar input:focus~ .search-btn{
+        outline: none;
+        border-right:2px solid #645cdd;
+        border-top:2px solid #645cdd;
+        border-bottom:2px solid #645cdd;
+    }
+
     .submissions-row{
         display: flex;
         flex-direction: row;
+        margin-right: 18px;
+        margin-left: 18px;
         /*background-color: pink;*/
     }
 
@@ -139,19 +247,98 @@ export default{
         margin:5px;
         flex:1;
         overflow: hidden;
-        height: 250px;
+        height: 150px;
         
     }
 
-    .submission-card{
+    .grp-card{
         cursor: pointer;
         width: 100%;
         height: 100%;
         border-radius: 3px;
-        border: 1px solid #ededed;
-        font-family: 'Roboto Condensed', sans-serif; 
+        border: 2px solid #ededed;
+        font-family: 'Lato', sans-serif; 
+        background-color: white;
+        color: #2c3e50;
+    }
+
+    .grp-card h6{
+        font-style: italic
+    }
+
+    .grp-card:hover{
+        background-color: #645cdd;
+        color: white;
+    }
+
+    .grp-card:active, .grp-card:focus{
+        background-color: #4d44bf;
+        color: white;
+
+    }
+
+    .grp-submission-row{
+        display: flex;
+        // background-color: blue;
+        min-height:400px;
+        margin: 20px;
+        animation-name: grow;
+        animation-duration: 0.3s;
+        animation-direction: normal;
+        transform-origin: top left;
+    }
+
+    @keyframes grow{
+        0%{
+            transform: scale(0);
+            opacity: 0;
+        }
+
+        100%{
+            transform: scale(1);
+            opacity: 1;
+        }
+    }
+
+    .grp-submission-row .container{
+        // background-color: pink;
+        max-width: 32%;
+        height: inherit;
+        margin-right: 18px;
+    }
+
+    .submission-card{
+        overflow: hidden;
+        min-height: inherit;
+        display: flex;
+        flex-direction: column;
         background-color: white;
     }
 
-</style>
+    .submission-card .image-area{
+        height: inherit;
+        padding: 10px;
+        // background-color: red;
+        
+    }
 
+    .submission-card img{
+        max-height: 400px;
+        max-width: 300px;
+        border-radius: 3px;
+        border: 2px solid #ededed;
+        font-family: 'Lato', sans-serif; 
+        background-color: white;
+        color: #2c3e50;
+        
+    }
+
+    .submission-details{
+        padding: 10px;
+        font-family: 'Lato', sans-serif;
+        
+    }
+
+
+
+</style>
