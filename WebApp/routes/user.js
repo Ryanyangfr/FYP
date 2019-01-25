12ba0f756950969dd549380a1948e816d8dd3e22
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-25 13:18:04 +0800

    Updated database script and cleaned up code in user.js

--- WebApp/routes/user.js
+++ WebApp/routes/user.js
@@ -1,30 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var Pusher = require('pusher')
-var crypto = require('crypto');
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Pusher = require('pusher');
+const crypto = require('crypto');
+const TokenGenerator = require('uuid-token-generator');
+const databaseConfig = require('../config/mysqlconf.js');
 
-var databaseConfig = require('../config/mysqlconf.js')
+const router = express.Router();
 
-var conn = mysql.createConnection(databaseConfig);
+const conn = mysql.createConnection(databaseConfig);
 
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-const TokenGenerator = require('uuid-token-generator');
+
 const tokenGen = new TokenGenerator();
 
-var pusher = new Pusher({
-    appId      : "650737",
-    key        : "1721c662be60b9cbd43c",
-    secret     : "a14c63eaaa50c1a466ca",
-    cluster    : "ap1",
-    encrypted  : true
+const pusher = new Pusher({
+  appId: '650737',
+  key: '1721c662be60b9cbd43c',
+  secret: 'a14c63eaaa50c1a466ca',
+  cluster: 'ap1',
+  encrypted: true
 });
 
-var channel = 'events_to_be_shown';
+const channel = 'events_to_be_shown';
 
 let numUsersEntered = 0;
 // checks if user is valid
@@ -50,119 +51,117 @@ let numUsersEntered = 0;
 //     })
 // })
 
-//for getting all users
-router.get('/retrieveAllUser', function(req,res){
-    var query = "SELECT * FROM PARTICIPANT";
-    console.log('User registered');
-    conn.query(query, function(err,result){
-        var respond_message = [];
-        if (err){
-            res.send("No participants has joined");
-        }
-
-        for(var i=0; i<result.length; i++){
-            respond_message.push({username: result[i].USERNAME, team: result[i].TEAM_ID, isLeader: result[i].isLeader});
-        }
-
-        console.log(respond_message);
-        res.send(JSON.stringify(respond_message));
-    })
-})
-
-//for registration, Accepts get request
-router.post('/register', function(req, res){
-    //gets parameters from post request
-    // console.log(req.body);
-    var username = req.body.username;
-    var user_id = numUsersEntered+1;
-    var team_id = numUsersEntered%3 + 1;
-    numUsersEntered = numUsersEntered+1;
-    var isLeader = 0;
-    if(user_id <= 3){
-        isLeader = 1;
+// for getting all users
+router.get('/retrieveAllUser', (req,res) => {
+  const query = 'SELECT * FROM PARTICIPANT';
+  console.log('User registered');
+  conn.query(query, (err,result) => {
+    const respond_message = [];
+    if (err) {
+      res.send('No participants has joined');
     }
-    var event = {
-        team_id: team_id,
-        id: crypto.randomBytes(16).toString('hex').substring(0,4)
-    };
 
+    for (let i = 0; i < result.length; i++) {
+      respond_message.push({ username: result[i].USERNAME, team: result[i].TEAM_ID, isLeader: result[i].isLeader });
+    }
 
+    console.log(respond_message);
+    res.send(JSON.stringify(respond_message));
+  });
+});
+
+// for registration, Accepts get request
+router.post('/register', (req, res) => {
+  // gets parameters from post request
+  // console.log(req.body);
+  const username = req.body.username;
+  const user_id = numUsersEntered + 1;
+  const team_id = numUsersEntered % 3 + 1;
+  numUsersEntered = numUsersEntered + 1;
+  let isLeader = 0;
+  if (user_id <= 3) {
+    isLeader = 1;
+  }
+  const event = {
+    team_id: team_id,
+    id: crypto.randomBytes(16).toString('hex').substring(0,4),
+  };
+
+  // console.log('request: ' + req);
+  // console.log('*****************************************************************')
+  // console.log('body: ' + req.body)
+  // console.log('*****************************************************************')
+
+  console.log(`username: ${username}`);
+  console.log(`user_id: ${user_id}`);
+  console.log(`team: ${team_id}`);
+  console.log(`user entered: ${numUsersEntered}`);
+  const query = 'INSERT INTO PARTICIPANT (USER_ID,USERNAME,TEAM_ID, isLeader) VALUES (?,?,?,?)';
+  // updates database
+  conn.query(query,[user_id,username,team_id, isLeader], (err,results) => {
+    if (err) {
+      console.log(err);
+      res.send('failed to update, check parameters');
+    } else {
+      pusher.trigger(channel, 'created', event);
+      res.send(event);
+    }
+  });
+});
+
+router.get('/getPassword', cors(), (req,res) => {
+  const username = req.query.username;
+
+  const query = 'SELECT USER_PASSWORD FROM ADMIN WHERE USERNAME = ?';
+
+  const token = tokenGen.generate();
+
+  console.log(token);
     
-    // console.log('request: ' + req);
-    // console.log('*****************************************************************')
-    // console.log('body: ' + req.body)
-    // console.log('*****************************************************************')
+  conn.query(query, username, (err, row) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('password sent');
+      res.send({ password: row[0].USER_PASSWORD, token: token });
+    }
+  });
+});
 
-    console.log('username: ' + username);
-    console.log('user_id: ' + user_id);
-    console.log('team: ' + team_id);
-    console.log('user entered: ' + numUsersEntered);
-    var query = "INSERT INTO PARTICIPANT (USER_ID,USERNAME,TEAM_ID, isLeader) VALUES (?,?,?,?)";
-    //updates database
-    conn.query(query,[user_id,username,team_id, isLeader], function(err,results){
-        if(err){
-            console.log(err);
-            res.send('failed to update, check parameters');
-        } else{
-            pusher.trigger(channel, 'created', event);
-            res.send(event);
-        }
-    })
-})
+router.post('/changeAdminPassword', cors(), (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const username = req.body.username;
+  const newPassword = req.body.password;
 
-router.get('/getPassword', cors(), function(req,res){
-    var username = req.query.username;
+  console.log('sent');
+  const query = 'UPDATE ADMIN SET USER_PASSWORD = ? WHERE USERNAME = ?';
 
-    query = "SELECT USER_PASSWORD FROM ADMIN WHERE USERNAME = ?";
+  conn.query(query, [newPassword, username], (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.send(JSON.stringify({ status: 'failed' }));
+    } else {
+      console.log('password updated');
+      res.send(JSON.stringify({ status: 'success' }));
+    }
+  });
+});
 
-    var token = tokenGen.generate();
+router.get('/retrieveAllUsers',(req,res) => {
+  const query = 'SELECT USERNAME FROM PARTICIPANT';
+  const response = [];
+  console.log('retrieveAllUsers called');
 
-    console.log(token);
-    
-    conn.query(query, username, function(err, row){
-        if (err){
-            console.log(err);
-        }else {
-            console.log("password sent");
-            res.send({password: row[0].USER_PASSWORD, token: token});
-        }
-    })
-})
+  conn.query(query, (err, rows) => {
+    if (err) {
+      console.log(error);
+    }
+    rows.forEach((row) => {
+      response.push(row.USERNAME);
+    });
 
-router.post('/changeAdminPassword', cors(), function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    var username = req.body.username;
-    var newPassword = req.body.password;
+    res.send(JSON.stringify({ username: response }));
+  });
+});
 
-    console.log('sent');
-    query = 'UPDATE ADMIN SET USER_PASSWORD = ? WHERE USERNAME = ?';
-
-    conn.query(query, [newPassword, username], function(err, rows){
-        if (err){
-            console.log(err);
-            res.send(JSON.stringify({'status': 'failed'}));
-        } else{
-            console.log('password updated');
-            res.send(JSON.stringify({'status': 'success'}));
-        }
-    })
-})
-
-router.get('/retrieveAllUsers',function(req,res){
-    query = 'SELECT USERNAME FROM PARTICIPANT';
-    response = []
-    console.log('retrieveAllUsers called');
-
-    conn.query(query, function(err, rows){
-        if (err){
-            console.log(error);
-        }
-        rows.forEach(function(row){
-            response.push(row.USERNAME);
-        })
-
-        res.send(JSON.stringify({username: response}));
-    })
-})
-
-module.exports=router;
+module.exports = router;
