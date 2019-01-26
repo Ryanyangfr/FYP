d7f875f7798f80b6957f1992c859fedd8fd149da
Ryanyangfr <42709812+Ryanyangfr@users.noreply.github.com>
2019-01-26 13:58:19 +0800

    Update index.vue

--- front_end_WebApp/vue_webapp/src/views/participant/index.vue
+++ front_end_WebApp/vue_webapp/src/views/participant/index.vue
@@ -4,15 +4,27 @@
             <input name="trailInstanceID" type="text" placeholder="Trail Instance ID" v-model="trailInstanceID">
             <button type="submit">Show Teams</button>
         </form> -->
-        
-        <table>
-
-        </table>
         <!-- {{teams}} -->
-        <div v-for="team in teams" :key="team">
-          <div v-for="member in team" :key="member">
-           {{member}}
-          </div>
+        <!-- {{allTeams}} -->
+        <div v-if="currTeamMember.length==0">
+            <div v-for="(team, index) in teams" :key="index">
+                Team
+                {{index+1}}
+                <v-select :options="team" v-model="currTeamMember" placeholder="Please select a member" style="width:300px;"></v-select>
+            </div>
+        </div>
+        <div v-if="currTeamMember.length!=0">
+            Current Team: 
+            {{currTeamMember.value}}
+            Member:
+            {{currTeamMember.label}}
+            <br><br>
+            <h3>Switch Team</h3>
+            <v-select :options="allTeams" v-model="newTeam" placeholder="Please select a new team" style="width:300px;"></v-select>
+            <br>
+            <button type="button" @click="switchTeam">Switch</button>
+            <button type="button" @click="deleteParticipant">Remove Participant</button>
+            <button type="button" @click="back">Back</button>
         </div>
     </div>
 </template>
@@ -22,16 +34,35 @@
 import vSelect from 'vue-select'
 import axios from 'axios'
 export default {
-  name: "hotspot",
+  name: "team",
   data() {
       return{
-        teams: []
+        teams: [],
+        allTeams: [],
+        currTeamMember: "",
+        newTeam: 0
       }
   }, 
   components:{
       vSelect
   },
   methods : {
+      back(){
+          this.currTeamMember = "";
+      },
+      
+      switchTeam(){
+            var postBody = {
+                "userID": this.currTeamMember.id,
+                "newTeam": this.newTeam
+            }
+            axios.post('http://54.255.245.23:3000/edit/switchTeams', postBody)
+            .then(response => {
+                let data = response.data;
+                console.log(data);
+                this.$router.go();
+          });
+      }
   },
   mounted() {
     axios.get('http://54.255.245.23:3000/team/getAllTeamsWithMembers')
@@ -46,8 +77,9 @@ export default {
                 let participant = row[ind];
                 console.log("participants: ");
                 console.log(participant);
-                temp.push(participant.username);
+                temp.push({label: participant.username, value: participant.team_id, id:participant.user_id});
               }
+              this.allTeams.push(parseInt(index)+1);
               this.teams.push(temp)
             }
         })
