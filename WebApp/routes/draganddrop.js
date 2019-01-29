166e3e3b2911b0ee45f131238f6b991fcc7c9db8
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-01-29 19:03:26 +0800

    Update draganddrop.js

--- WebApp/routes/draganddrop.js
+++ WebApp/routes/draganddrop.js
@@ -23,7 +23,7 @@ router.get('/getDragAndDrop',function(req,res){
                 var mission = missionRow.MISSION_ID;
                 var hotspot_name = missionRow.HOTSPOT_NAME;
                 // console.log(mission)
-                mission_query = 'SELECT DRAGANDDROP_QUESTION, DRAGANDDROP_ANSWER FROM DRAG_AND_DROP WHERE MISSION_ID = ?';
+                mission_query = 'SELECT DRAGANDDROP_QUESTION, DRAGANDDROP_QUESTION_OPTION, DRAGANDDROP_QUESTION_ANSWER FROM DRAG_AND_DROP, DRAG_AND_DROP_OPTION WHERE DRAG_AND_DROP.DRAGANDDROP_ID = DRAG_AND_DROP_OPTION.DRAGANDDROP_ID AND MISSION_ID = ?';
 
                 conn.query(mission_query, mission, function(err, drag_and_drop_details){
                     console.log(mission);
@@ -36,10 +36,10 @@ router.get('/getDragAndDrop',function(req,res){
                         drag_and_drop_ = []
                         if (drag_and_drop_details.length != 0){
                             drag_and_drop_details.forEach(function(detail, i){
-                                drag_and_drop_.push({drag_and_drop_question:detail.DRAGANDDROP_QUESTION,  drag_and_drop_answer: detail.DRAGANDDROP_ANSWER})
+                                drag_and_drop_.push({drag_and_drop_question:detail.DRAGANDDROP_QUESTION_OPTION,  drag_and_drop_answer: detail.DRAGANDDROP_QUESTION_ANSWER})
                             })
                             // console.log(drag_and_drop_)
-                            response.push({hotspot: hotspot_name, drag_and_drop: drag_and_drop_});
+                            response.push({hotspot: hotspot_name, question: drag_and_)and_drop_details[0].DRAGANDDROP_QUESTION, drag_and_drop: drag_and_drop_});
                             console.log('count: ' + typeof count);
                             console.log('mission length: ' + typeof missions.length);
                             if(count == missions.length){
