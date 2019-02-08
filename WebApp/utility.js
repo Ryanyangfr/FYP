a8c3dcc799e1bc4088ac171be88d83075ca781b6
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-02-08 18:34:48 +0800

    Update utility.js

--- WebApp/utility.js
+++ WebApp/utility.js
@@ -1,25 +1,27 @@
-function getDateTime() {
+module.exports = {
+  method: function getDateTime() {
 
-    var date = new Date();
+    const date = new Date();
+    date.setTime(date.getTime() + date.getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000);
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
+    let hour = date.getHours();
+    hour = (hour < 10 ? '0' : '') + hour;
 
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
+    let min = date.getMinutes();
+    min = (min < 10 ? '0' : '') + min;
 
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
+    let sec = date.getSeconds();
+    sec = (sec < 10 ? '0' : '') + sec;
 
-    var year = date.getFullYear();
+    const year = date.getFullYear();
 
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? '0' : '') + month;
 
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
+    let day = date.getDate();
+    day = (day < 10 ? '0' : '') + day;
 
-    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+    return `${year}:${month}:${day}:${hour}:${min}:${sec}`;
 
+  }
 }
-module.exports = getDateTime
