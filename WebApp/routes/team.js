10400e9b52846ae8cc69c8c3bd0289e41753ce95
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-02-08 18:14:27 +0800

    Update team.js

--- WebApp/routes/team.js
+++ WebApp/routes/team.js
@@ -33,7 +33,23 @@ router.post('/updateScore', (req, res) => {
   
   const today = new Date();
   const date = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
-  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+ 
+  let minutes = today.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  
+  let seconds = today.getSeconds();
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  let hours = today.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  const time = `${hours}:${minutes}:${seconds}`;
   const dateTime = `${date} ${time}`;
   console.log(`timestamp: ${dateTime}`);
   // console.log('time: ' + time);
