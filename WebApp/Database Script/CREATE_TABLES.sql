5283a5e0af6eb4b848edb748e89e06d9919f1967
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-02-14 22:44:30 +0800

    Update CREATE_TABLES.sql

--- WebApp/Database Script/CREATE_TABLES.sql
+++ WebApp/Database Script/CREATE_TABLES.sql
@@ -497,7 +497,7 @@ INSERT INTO TRAIL_HOTSPOT VALUES
 (1, 'School of Accountancy', 2, 4),
 -- (1, 'School of Economics/School of Social Sciences', 3, 2),
 -- (1, 'School of Information Systems', 4, 1),
-(1, 'School of Law', 5, 6);
+(1, 'School of Law', 5, 6),
 (2, 'Li Ka Shing Library', 6, 5),
 (2, 'Lee Kong Chian School of Business', 1, 3),
 (2, 'School of Accountancy', 2, 4);
