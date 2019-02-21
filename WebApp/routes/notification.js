da9e5ca923bccb6693959a9bbb0879d9dfc09db0
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-02-21 13:46:15 +0800

    Update notification.js

--- WebApp/routes/notification.js
+++ WebApp/routes/notification.js
@@ -11,9 +11,9 @@ const conn = mysql.createConnection(databaseConfig);
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get('/sendNotification', (req,res) => {
+router.post('/sendNotification', (req,res) => {
   const io = req.app.get('socketio');
-  const message = req.query.message;
+  const message = req.body.message;
   console.log(message)
 
   io.emit('notification', {message});
