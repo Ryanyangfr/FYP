8392b60c917b693c116d0fecc21c127501c4cd5a
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-02-25 16:54:21 +0800

    Update team.js

--- WebApp/routes/team.js
+++ WebApp/routes/team.js
@@ -221,6 +221,7 @@ router.post('/teamLocation', (req,res) => {
   router.get('/getAllTeamPoints', (req,res) => {
     // get current trail instance
     const activeTrailInstanceID = utility.getActiveTrailInstanceID(conn);
+    console.log(`instance id : ${activeTrailInstanceID}`)
     const response = [];
     const getActiveTrailInstance = 'SELECT TRAIL_INSTANCE_ID FROM TRAIL_INSTANCE WHERE ISACTIVE = 1';
 
