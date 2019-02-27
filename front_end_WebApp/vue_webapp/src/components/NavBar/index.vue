e62a2fd9718b2d5b8ae0629daaae1d6ba4492c42
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-02-27 21:34:09 +0800

    Update index.vue

--- front_end_WebApp/vue_webapp/src/components/NavBar/index.vue
+++ front_end_WebApp/vue_webapp/src/components/NavBar/index.vue
@@ -5,6 +5,15 @@
                 {{$route.name}}
             </div>
             <div class="trail-id-area">
+                Time Left: 
+                <div v-if = "this.seconds >= 0">
+                    {{this.hours}}:{{this.minutes}}:{{this.seconds}}
+                </div>
+                <div v-else>
+                    00:00:00
+                </div>
+            </div>
+            <div class="trail-id-area">
                 Current Trail ID: 
                 <div v-if="this.$store.state.currentTrailID==='-'">{{this.trail_instance_id}}</div>
                 <div v-else>{{this.$store.state.currentTrailID}}</div>
@@ -20,15 +29,47 @@ export default{
     name: 'Navigation',
     data(){
         return{
-            trail_instance_id:"-"
+            trail_instance_id:"-",
+            start: "",
+            end: "",
+            interval: "",
+            minutes: 0,
+            hours: 0,
+            seconds: 0
         }
     },
 
     computed: {
         currentTrailID(){
             return this.$store.state.currentTrailID;
+        },
+        instanceStartTime(){
+            return this.$store.state.instanceStartTime;
         }
-    },   
+    },
+    
+    methods: {
+        calcTime(dist){
+            // Time calculations for days, hours, minutes and seconds
+            this.hours = Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            this.minutes = Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
+            this.seconds = Math.floor((dist % (1000 * 60)) / 1000);
+        },
+        timerCount(){
+            // Get todays date and time
+            let now = new Date().getTime();
+            // console.log(start);
+            // console.log(end);
+            // Find the distance between now an the count down date
+            // var distance = start - now;
+            var passTime =  this.$store.state.instanceStartTime - now;
+            // console.log(passTime)
+            this.calcTime(passTime);
+            if (this.seconds == 0) {
+                alert("Trail has ended");
+            }
+        }
+    },
 
     mounted() {
         axios.get('http://54.255.245.23:3000/getCurrentTrailInstanceID')
@@ -39,6 +80,18 @@ export default{
                 this.trail_instance_id  = data[row]
             }
         })
+
+        // this.end = new Date(this.start + 60*60*1000).getTime();
+        // Update the count down every 1 second
+        // this.timerCount(this.start,this.end);
+        // console.log(this.start);
+        // console.log(this.end);
+        this.interval = setInterval(() => {
+            // console.log(this.$store.state.instanceStartTime);
+            this.timerCount();
+        }, 1000);
+
+        // console.log(this.interval)
     }
 }
 </script>
@@ -76,6 +129,7 @@ export default{
         font-size: 18px;
         padding-top: 5px;
         padding-bottom: 5px;
+        padding-left: 5px;
         color:black;
         font-family: 'Lato', sans-serif;
         /* background-color: pink; */
