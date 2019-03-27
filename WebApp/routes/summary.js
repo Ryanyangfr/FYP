151a9c434b8117c8c9207dbb54d19b0279fa20a8
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-03-27 21:41:07 +0800

    Update summary.js

--- WebApp/routes/summary.js
+++ WebApp/routes/summary.js
@@ -11,6 +11,7 @@ router.get('/getSummaryReport', (req,res) => {
   const response = [];
   const query ='SELECT * FROM SUMMARY_TABLE';
   const getDateQuery = 'SELECT DATE_STARTED FROM TRAIL_INSTANCE WHERE TRAIL_INSTANCE_ID = ?';
+  const getNumParticipantsQuery = 'SELECT COUNT(*) AS COUNT FROM PARTICIPANT WHERE TRAIL_INSTANCE_ID = ?';
   let count = 0;
 
   conn.query(query, (err,data) => {
@@ -23,19 +24,28 @@ router.get('/getSummaryReport', (req,res) => {
         let hotspot = row.HOTSPOT_NAME;
         conn.query(getDateQuery, trailInstanceID, (err,data2) => {
           if (err) {
-            console.log(err)
+            console.log(err);
             res.send(response);
           } else {
             let date = data2[0].DATE_STARTED;
-            response.push({trailInstanceID, hotspot, date});
-            count += 1;
-            if(count === data.length){
-              res.send(response);
-            }
+            conn.query(getNumParticipantsQuery, trailInstanceID, (err, data3) => {
+              if (err) {
+                console.log(err);
+                res.send(response);
+              } else {
+                const participants = data3[0].COUNT;
+                response.push({trailInstanceID, hotspot, date, participants});
+                count += 1;
+                if (count === data.length) {
+                  res.send(response);
+                }
+              }
+            });
+            
           }
-        })
-      })
+        });
+      });
     }
-  })
-})
+  });
+});
 module.exports = router;
