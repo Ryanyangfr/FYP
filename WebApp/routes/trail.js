1ab0295a6565d8ac51263fb78b3cf86b7a33afa4
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-03-27 15:47:57 +0800

    Update trail.js

--- WebApp/routes/trail.js
+++ WebApp/routes/trail.js
@@ -363,7 +363,7 @@ function duplicateQuiz(trailInstanceID, missionHistoryID, insertMissionHistoryQu
               result3.forEach((row) => {
                 rowIndex += 1;
                 console.log(`row index top: ${rowIndex}`)
-                if (rowIndex % 4 == 0) {
+                if (rowIndex % 4 === 0) {
                   // console.log(row);
                   const hotspot = row.HOTSPOT_NAME;
                   const missionID = row.MISSION_ID;
