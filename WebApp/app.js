06b3310ea4299d460bcb0bddf3b1c9c8f2dfcd20
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-04-04 15:04:18 +0800

    Update app.js

--- WebApp/app.js
+++ WebApp/app.js
@@ -42,6 +42,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://www.amazingtrail.ml');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
