d14a3a9da347f03139934ed8bfbe61b3d8edae1c
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-04-09 19:47:33 +0800

    Update deleteElements.js

--- WebApp/routes/deleteElements.js
+++ WebApp/routes/deleteElements.js
@@ -54,7 +54,7 @@ router.post('/deleteQuiz', (req, res) => {
       const deleteQuizQuery = 'DELETE FROM QUIZ WHERE MISSION_ID = ?';
       const deleteMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
       let count = 0;
-      let quizCompleted = 0
+      let quizCompleted = 0;
       let anyErr = false;
 
       rows.forEach((row) => {
@@ -99,13 +99,31 @@ router.post('/deleteQuiz', (req, res) => {
 router.post('/deleteWefieQuestion', (req, res) => {
   const wefieID = req.body.id;
   const query = 'DELETE FROM SUBMISSION_QUESTION WHERE QUESTION_ID = ?';
+  const deleteFromMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
+  const getMissionID = 'SELECT MISSION.MISSION_ID FROM SUBMISSION_QUESTION, MISSION WHERE MISSION.MISSION_ID = SUBMISSION_QUESTION.MISSION_ID AND QUESTION_ID = ?';
+
 
   conn.query(query, wefieID, (err, data) => {
     if (err) {
       console.log(err);
       res.send(JSON.stringify({ success: 'false' }));
     } else {
-      res.send(JSON.stringify({ success: 'true' }));
+      conn.query(getMissionID, wefieID, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send(JSON.stringify({ success: 'false' }));
+        } else {
+          const missionID = data[0].MISSION_ID;
+          conn.query(deleteFromMissionQuery, missionID, (err, data2) => {
+            if (err) {
+              console.log(err);
+              res.send(JSON.stringify({ success: 'false' }));
+            } else {
+              res.send(JSON.stringify({ success: 'true' }));
+            }
+          });
+        }
+      });
     }
   });
 });
@@ -148,23 +166,40 @@ router.post('/deleteDragAndDrop', (req,res) => {
             } else {
               res.send(JSON.stringify({ success: 'true' }));
             }
-          })
+          });
         }
-      })
+      });
     }
-  })
-})
+  });
+});
 
 router.post('/deleteDrawingQuestion', (req, res) => {
   const drawingID = req.body.id;
   const query = 'DELETE FROM DRAWING_QUESTION WHERE QUESTION_ID = ?';
+  const deleteFromMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
+  const getMissionID = 'SELECT MISSION.MISSION_ID FROM DRAWING_QUESTION, MISSION WHERE MISSION.MISSION_ID = DRAWING_QUESTION.MISSION_ID AND QUESTION_ID = ?';
 
   conn.query(query, drawingID, (err, data) => {
     if (err) {
       console.log(err);
       res.send(JSON.stringify({ success: 'false' }));
     } else {
-      res.send(JSON.stringify({ success: 'true' }));
+      conn.query(getMissionID, drawingID, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send(JSON.stringify({ success: 'false' }));
+        } else {
+          const missionID = data[0].MISSION_ID;
+          conn.query(deleteFromMissionQuery, missionID, (err, data2) => {
+            if (err) {
+              console.log(err);
+              res.send(JSON.stringify({ success: 'false' }));
+            } else {
+              res.send(JSON.stringify({ success: 'true' }));
+            }
+          });
+        }
+      });
     }
   });
 });
@@ -172,13 +207,30 @@ router.post('/deleteDrawingQuestion', (req, res) => {
 router.post('/deleteAnagram', (req, res) => {
   const anagramID = req.body.id;
   const query = 'DELETE FROM ANAGRAM WHERE ANAGRAM_ID = ?';
+  const deleteFromMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
+  const getMissionID = 'SELECT MISSION.MISSION_ID FROM ANAGRAM, MISSION WHERE MISSION.MISSION_ID = ANAGRAM.MISSION_ID AND ANAGRAM_ID = ?';
 
   conn.query(query, anagramID, (err, data) => {
     if (err) {
       console.log(err);
       res.send(JSON.stringify({ success: 'false' }));
     } else {
-      res.send(JSON.stringify({ success: 'true' }));
+      conn.query(getMissionID, drawingID, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send(JSON.stringify({ success: 'false' }));
+        } else {
+          const missionID = data[0].MISSION_ID;
+          conn.query(deleteFromMissionQuery, missionID, (err, data2) => {
+            if (err) {
+              console.log(err);
+              res.send(JSON.stringify({ success: 'false' }));
+            } else {
+              res.send(JSON.stringify({ success: 'true' }));
+            }
+          });
+        }
+      });
     }
   });
 });
@@ -187,6 +239,9 @@ router.post('/deleteWordsearch', (req, res) => {
   const wordSearchID = req.body.id;
   const wordSearchQuery = 'DELETE FROM WORDSEARCH WHERE WORDSEARCH_ID = ?';
   const wordSearchWordQuery = 'DELETE FROM WORDSEARCH_WORD WHERE WORDSEARCH_ID = ?';
+  const deleteFromMissionQuery = 'DELETE FROM MISSION WHERE MISSION_ID = ?';
+  const getMissionID = 'SELECT MISSION.MISSION_ID FROM WORDSEARCH, MISSION WHERE MISSION.MISSION_ID = WORDSEARCH.MISSION_ID AND WORDSEARCH_ID = ?';
+
 
   conn.query(wordSearchWordQuery, wordSearchID, (err, data) => {
     if (err) {
@@ -199,8 +254,8 @@ router.post('/deleteWordsearch', (req, res) => {
         } else {
           res.send(JSON.stringify({ success: 'true' }));
         }
-      })
+      });
     }
-  })
-})
+  });
+});
 module.exports = router;
